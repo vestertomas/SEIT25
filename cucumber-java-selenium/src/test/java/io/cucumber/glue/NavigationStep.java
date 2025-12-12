0bee49ef4f875e964180ac407206f0fd8ba8d9a8
vestertomas <83171715+vestertomas@users.noreply.github.com>
2025-12-12 19:29:08 +0800

    Updated NavigationStep

--- cucumber-java-selenium/src/test/java/io/cucumber/glue/NavigationStep.java
+++ cucumber-java-selenium/src/test/java/io/cucumber/glue/NavigationStep.java
@@ -11,7 +11,6 @@ import io.cucumber.pages.HomePage;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NavigationStep extends Context {
   public NavigationStep(Manager manager) {
